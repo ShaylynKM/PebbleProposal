458ef9748446d2261b9de691b094d35ba11b669f
tara <t.dinreinwald512@mybvc.ca>
2023-07-19 09:52:15 -0600

    Revert "added more things"
    This reverts commit 6b29d32aa7894530bfbf1c20da8a6701cf4d25d0.

--- Assets/Scripts/Rocks.cs
+++ Assets/Scripts/Rocks.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Rocks : MonoBehaviour
 {
-    public string itemId; // Unique identifier for the collectible item
+    public int itemId; // Unique identifier for the collectible item
     public Sprite itemSprite; // Sprite or model representing the collectible item
     public string itemDescription; // Description of the collectible item
+    public bool isCollected;
+
+    private void Start()
+    {
+        isCollected = false;
+    }
 
     // When the player interacts with the collectible item
-    private void OnTriggerStay2D(Collider2D collision)
+    public void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
@@ -17,13 +23,16 @@ public class Rocks : MonoBehaviour
             {
                 Debug.Log("Item ID: " + itemId); // Add this line to print the item ID
 
+                isCollected = true;
                 // Add picking rock up animation and sound effect
 
                 // Add the item to the player's inventory
-                PlayerInventory.instance.AddItem(itemId);
+                //PlayerInventory.instance.AddItem(itemId);
 
                 // Destroy the collectible item
                 Destroy(gameObject);
+
+                Debug.Log(isCollected);
             }
         }
     }
