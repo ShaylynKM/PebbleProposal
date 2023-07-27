2b793ea51b60e85ea8128fe0a3166451c929ba07
annavgk <annavgk@gmail.com>
2023-07-26 22:20:52 -0600

    Fixed Menus in Level1
    Started audios but didn't get through

--- Assets/Scripts/Audio_Scripts/AudioManager.cs
+++ Assets/Scripts/Audio_Scripts/AudioManager.cs
@@ -6,5 +6,29 @@ using System;
 
 public class AudioManager : MonoBehaviour
 {
-    
+    public AudioSource buttonSound;
+    public AudioSource jumpSound;
+    public AudioSource backgroundSound;
+
+    [SerializeField] AudioClip buttonClip;
+    [SerializeField] AudioClip jumpClip;
+    [SerializeField] AudioClip backgroundClip;
+
+    public void OnButtonClick()
+    {
+        buttonSound.clip = buttonClip;
+        buttonSound.Play();
+    }
+
+    public void OnJump()
+    {
+        jumpSound.clip = jumpClip;
+        jumpSound.Play();
+    }
+
+    public void OnPlayingBG()
+    {
+        backgroundSound.clip = backgroundClip;
+        backgroundSound.Play();
+    }
 }
--- Assets/Scripts/UI_Scripts/MenuThings.cs
+++ Assets/Scripts/UI_Scripts/MenuThings.cs
@@ -52,24 +52,6 @@ public class MenuThings : MonoBehaviour
         SceneManager.LoadScene("Level1");
     }
 
-    //public void OnSettingsButton()
-    //{
-    //    if(GameObject.Find("SettingsMenu").activeSelf == true)
-    //    {
-    //        if(
-    //        {
-    //            GameObject.Find("SettingsMenu").SetActive(false);
-    //        }
-    //    }
-
-    //    if (GameObject.Find("SettingsMenu").activeSelf == false)
-    //    {
-    //        if (Input.GetKeyDown("return"))
-    //        {
-    //            GameObject.Find("SettingsMenu").SetActive(true);
-    //        }
-    //    }
-    //}
 
     public void OnExitButton()
     {
